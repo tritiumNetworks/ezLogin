2319d54d36542f8b3883de782f209076c458620b
PMHStudio <pmhstudio.pmh@gmail.com>
2019-11-23 14:54:58 +0900

    Example Code Finished

--- example/index.js
+++ example/index.js
@@ -1,54 +1,57 @@
 const { AccountBase, version } = require('../index')
 const { resolve } = require('path')
-const { unlinkSync } = require('fs')
+const prompts = require('prompts')
 
 console.log('quickLogin v' + version)
 
-unlinkSync(resolve() + '/example/test.json')
 const db = new AccountBase({ save: true, path: resolve() + '/example/test.json' })
 
-switch (db.register('pmh_only', 'nitrogen123')) {
-  case 0: console.log('Registered Sucessfully'); break
-  case 1: console.log('Please Enter your id & passwd'); break
-  case 2: console.log('Account Base is not loading yet'); break
-  case 3: console.log('this id has been taken'); break
-  default: console.log('There is an Error!'); break
-}
-console.log(JSON.stringify(db.accounts) + '\n')
+proc()
 
-switch (db.register('pmh_only', 'nitrogen123')) {
-  case 0: console.log('Registered Sucessfully'); break
-  case 1: console.log('Please Enter your id & passwd'); break
-  case 2: console.log('Account Base is not loading yet'); break
-  case 3: console.log('this id has been taken'); break
-  default: console.log('There is an Error!'); break
-}
-
-switch (db.login('pmh_only', 'nitrogen123')) {
-  case 0: console.log('Login Correct'); break
-  case 1: console.log('Please Enter your id & passwd'); break
-  case 2: console.log('Account Base is not loading yet'); break
-  case 3: console.log('ID is not Correct'); break
-  case 4: console.log('Passwd is not Correct'); break
-  default: console.log('There is an Error!'); break
+function join () {
+  prompts([
+    { type: 'text', name: 'id', message: 'Please enter your ID' },
+    { type: 'password', name: 'pw', message: 'Please enter your PassWord' }
+  ]).then((aws) => {
+    switch (db.register(aws.id, aws.pw)) {
+      case 0: console.log('Registered Sucessfully ' + JSON.stringify(db.accounts)); break
+      case 1: console.log('Please Enter your id & passwd'); break
+      case 2: console.log('Account Base is not loading yet'); break
+      case 3: console.log('this id has been taken'); break
+      default: console.log('There is an Error!'); break
+    }
+    proc()
+  })
 }
 
-switch (db.login('pmhonly', 'nitrogen123')) {
-  case 0: console.log('Login Correct'); break
-  case 1: console.log('Please Enter your id & passwd'); break
-  case 2: console.log('Account Base is not loading yet'); break
-  case 3: console.log('ID is not Correct'); break
-  case 4: console.log('Passwd is not Correct'); break
-  default: console.log('There is an Error!'); break
+function login () {
+  prompts([
+    { type: 'text', name: 'id', message: 'Please enter your ID' },
+    { type: 'password', name: 'pw', message: 'Please enter your PassWord' }
+  ]).then((aws) => {
+    switch (db.login(aws.id, aws.pw)) {
+      case 0: console.log('Login Correct'); break
+      case 1: console.log('Please Enter your id & passwd'); break
+      case 2: console.log('Account Base is not loading yet'); break
+      case 3: console.log('ID is not Correct'); break
+      case 4: console.log('Passwd is not Correct'); break
+      default: console.log('There is an Error!'); break
+    }
+    proc()
+  })
 }
 
-switch (db.login('pmh_only', 'nitrogen1234')) {
-  case 0: console.log('Login Correct'); break
-  case 1: console.log('Please Enter your id & passwd'); break
-  case 2: console.log('Account Base is not loading yet'); break
-  case 3: console.log('ID is not Correct'); break
-  case 4: console.log('Passwd is not Correct'); break
-  default: console.log('There is an Error!'); break
+function proc () {
+  prompts({
+    type: 'select',
+    name: 'f',
+    message: 'what\'s up?',
+    choices: [
+      { title: 'Join', value: join },
+      { title: 'Log in', value: login },
+      { title: 'exit', value: process.exit }
+    ]
+  }).then((aws) => {
+    aws.f()
+  })
 }
-
-db.break()
